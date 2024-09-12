0c8f6e0720926b23024d1315d41016f2955af54c
Julian Kühn <kuehnjuli@gmail.com>
2024-09-12 18:21:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function Heart({ xPos, yPos, size }) {
+  return (
+    <div
+      className="heart"
+      style={{
+        left: `${xPos}%`,
+        top: `${yPos}%`,
+        width: `${size}px`,
+        height: `${size}px`,
+      }}
+    ></div>
+  );
+}
 
 function App() {
+  const [hearts, setHearts] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newHeart = {
+        id: Math.random(),
+        xPos: Math.random() * 100,
+        yPos: Math.random() * 100,
+        size: Math.random() * 35 + 20, // Herzgrößen zwischen 20px und 60px
+      };
+      setHearts((prev) => [...prev, newHeart]);
+
+      // Herzen nach einer Zeit entfernen
+      setTimeout(() => {
+        setHearts((prev) => prev.filter((heart) => heart.id !== newHeart.id));
+      }, 3000); // Herzen verschwinden nach 3 Sekunden
+    }, 500); // Alle 0.5 Sekunden ein neues Herz hinzufügen
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      {hearts.map((heart) => (
+        <Heart key={heart.id} xPos={heart.xPos} yPos={heart.yPos} size={heart.size} />
+      ))}
+    </div>
+    <div className="Login">
+      <h4>Du kennst das Passwort...</h4>
+      <input className="input" type="number" value={inputValue} onChange={handleInputChange} />
+      <div>
+        <button className="btn-weiter">Weiter</button>
+      </div>
+    </div>
     </div>
   );
 }
